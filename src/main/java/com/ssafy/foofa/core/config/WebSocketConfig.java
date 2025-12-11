46b6957219c1c87106688c2d0d8b247529d8244a
codrin2 <hssang127@gmail.com>
2025-12-11 17:11:18 +0900

    feat: WebSocket STOMP 엔드포인트 및 메시지 브로커 설정 추가 #4

---
+++ src/main/java/com/ssafy/foofa/core/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.ssafy.foofa.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 클라이언트로 메시지 전송하는 prefix
+        registry.enableSimpleBroker("/topic");
+
+        // 클라이언트에서 메시지 전송하는 prefix
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
