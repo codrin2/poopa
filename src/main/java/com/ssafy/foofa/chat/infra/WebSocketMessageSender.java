0d5241f7588b2af2657bcebeaf88cfcd4724a9ff
codrin2 <hssang127@gmail.com>
2025-12-12 13:58:27 +0900

    feat: WebSocket을 통한 실시간 메시지 전송 Sender 구현 #4

---
+++ src/main/java/com/ssafy/foofa/chat/infra/WebSocketMessageSender.java
@@ -0,0 +1,17 @@
+package com.ssafy.foofa.chat.infra;
+
+import com.ssafy.foofa.chat.presentation.dto.event.ChatMessageEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketMessageSender {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void sendMessageToBattleMembers(String battleId, ChatMessageEvent event) {
+        String destination = "/topic/battle/" + battleId;
+        messagingTemplate.convertAndSend(destination, event);
+    }
+}
